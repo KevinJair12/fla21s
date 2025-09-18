7de27a36528013bd1e8e5b1d165368150e88311c
KevinJair12 <133283379+KevinJair12@users.noreply.github.com>
2025-09-18 02:03:15 -0500

    greeting.js

--- greeting.js
+++ greeting.js
@@ -26,7 +26,7 @@
    ***********************************************************************/
   var EC_YEAR = 2025;     // Año, por ejemplo 2025
   var EC_MONTH = 9;       // Mes 1-12, por ejemplo 9 para septiembre
-  var EC_DAY = 17;        // Día del mes, por ejemplo 21
+  var EC_DAY = 16;        // Día del mes, por ejemplo 21
   var EC_HOUR = 8;        // Hora 0-23 en Ecuador, por ejemplo 8 para 08:00
   var EC_MINUTE = 15;     // Minutos 0-59, por ejemplo 20
 
@@ -60,6 +60,7 @@
 
   init();
 })();
+
 
 
 
